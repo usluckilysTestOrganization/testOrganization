c99720cef894531856e444ea048a89b9ec950383
usluckily <763806142@qq.com>
2017-05-08 18:50:41 +0800

    random circle...

--- src/components/loader/randomCircle.vue
+++ src/components/loader/randomCircle.vue
@@ -2,6 +2,8 @@
   <canvas id="randomCircle" width=""  height=""></canvas>
 </template>
 <script>
+  import $ from 'jquery'
+
   export default({
     name:'randomCircle',
     data(){
@@ -16,23 +18,46 @@
         let rc = document.getElementById('randomCircle'),
           _this = this,
           cxt  = rc.getContext('2d'),
-          rNum = Math.random()*10,
-          w = screen.clientWidth,
-          h = screen.clientHeight;
+          rNum = _this.random(15,5),
+          w = $(window).width(),
+          h = $(window).height();
 
         _this.w = w;
         _this.h = h;
         rc.width = w;
         rc.height = h;
 
+        let circles = []//存放圆的数组
+
         for(let i = 0;i<rNum;i++){
-          cxt.fillStyle="rgba(156,234,128,70%)";
+          let x = _this.random(w,0),//坐标x
+              y = _this.random(h,0),//坐标y
+              radius = _this.random(15,10),//半径
+              xa = _this.random(2,-1),//x轴加速度
+              ya = _this.random(2,-1);//y轴加速度
+          //随机生成圆的各种参数
+
+          circles.push({
+            x:x,
+            y:y,
+            radius:radius,
+            xa:xa,
+            ya:ya
+          })
+        }
+
+        for(let i in circles){
+          cxt.fillStyle="rgba("+_this.random(255,0)+","+_this.random(255,0)+","+_this.random(255,0)+",40%)";
           cxt.beginPath();
-          cxt.arc( Math.random()*600+60 , Math.random()*1000+100 , Math.random()*40+20 ,0,Math.PI*2,true);
+          cxt.arc( circles[i].x , circles[i].y , circles[i].radius ,0,Math.PI*2,true);
           cxt.closePath();
           cxt.fill();
         }
+        //根据circles生成圆
 
+      },
+      random(x,y){
+        return Math.random()*x+y
       }
     },
     mounted(){
@@ -41,5 +66,5 @@
   })
 </script>
 <style>
-  #randomCircle{position:fixed;z-index:-1;border:1px solid #000}
+  #randomCircle{position:fixed;z-index:-1;}
 </style>
